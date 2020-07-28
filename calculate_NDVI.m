cdf5c432a346a4c5583fa1da26f39867fc50881a
Sebbyraft <alessandroseba1995@gmail.com>
2020-07-28 14:29:51 +0200

    🌱 NDVI computing function

--- calculate_NDVI.m
+++ calculate_NDVI.m
@@ -1,8 +1,10 @@
 function [ndvi] = calculate_NDVI(data, bands)
 %CALCULATE_NDVI calculate the NDVI index
 %NDVI = (NIR - VIS)/(NIR + VIS)
-%bands must be a vector with the index, for the data matrix, of the NIR
+%-data: multispectral Satellite data
+%-bands: vector with the index, for the data matrix, of the NIR
 %and VIS bands.
+% https://it.wikipedia.org/wiki/Normalized_Difference_Vegetation_Index
 
 if nargin < 2
     disp('Invalid arguments error: data and bands must be specified');
