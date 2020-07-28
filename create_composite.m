69c2fc4eb02dee3424a4d869d417bcf1eba2b115
Sebbyraft <alessandroseba1995@gmail.com>
2020-07-28 14:42:47 +0200

    🎨 create a composite

--- create_composite.m
+++ create_composite.m
@@ -9,12 +9,17 @@ function [composite] = create_composite(data, bands)
 %[4 3 2] -> B4 (Red band), B3 (Green band), B2 (Blue band)
 %Sentinel-2 Bands -> [B1, B2, B3, B4, B5, B6, B7, B8, B8A, B9, B10, B11, B12, QA10, QA20, QA60]
 
-    s = size(data);
-    composite = zeros(s(1), s(2), length(bands));
+if nargin < 2
+    disp('Invalid arguments error: data and bands must be specified');
+    return;
+end
 
-    for i = 1:length(bands)
-        composite(:,:,i) = data(:,:,bands(i));
-    end
+s = size(data);
+composite = zeros(s(1), s(2), length(bands));
+
+for i = 1:length(bands)
+    composite(:,:,i) = data(:,:,bands(i));
+end
 
 end
 
