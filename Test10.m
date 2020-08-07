480d73e80405eace0e061d4b7a137a4eedf9a45a
Sebbyraft <alessandroseba1995@gmail.com>
2020-08-07 17:36:07 +0200

    📝 Time-series test

--- Test10.m
+++ Test10.m
@@ -1,12 +1,17 @@
 clear all; close all; clc;
 
+%% Load images paths
 fileList = dir('data/Sentinel-2_Timeseries/*.tif');
 
 s = size(fileList);
-
 ndvis = zeros(s(1),2);
-
 labels = strings(s(1),1);
+
+%% Read and process data 
+% -1: read
+% -2: normalize data
+% -3: crop
+% -4: calculate NDVI
 
 for i=1:s(1)
    
@@ -18,18 +23,20 @@ for i=1:s(1)
     
     cropped_data = crop(normalized_data, [100, 150, 100, 150]);
     
-    ndvi = 2*calculate_NDVI(cropped_data, [8 4]) -0.5;
+    ndvi = 2*calculate_NDVI(cropped_data, [8 4])-0.5;
     
-    ndvis(i, 1) = max(ndvi(:));
+    ndvis(i, 1) = mean(ndvi(:));
     ndvis(i, 2) = min(ndvi(:));
       
 end
 
+%% Plot results
 figure, 
 x = linspace (1, s(1), s(1));
-plot(x, ndvis(:, 1), '.r', 'MarkerSize', 20); hold on; grid on;
-plot(x, ndvis(:, 2), '.g', 'MarkerSize', 20);
-
+plot(x, ndvis(:, 1), '.', 'MarkerSize', 20); hold on; grid on;
 xticklabels(labels)
+xticks(x)
+xtickangle(90)
+title('Spatial Mean of NDVI for 1 year time-series', 'FontSize', 24);
 
 
