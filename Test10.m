bf35a0264c06e6e77ee4ba63c758a51b23415670
Sebbyraft <alessandroseba1995@gmail.com>
2020-08-07 16:37:43 +0200

    🗃  Sentinel-1 and -2 data

--- Test10.m
+++ Test10.m
@@ -3,19 +3,33 @@ clear all; close all; clc;
 fileList = dir('data/Sentinel-2_Timeseries/*.tif');
 
 s = size(fileList);
-img_path = strcat('data/Sentinel-2_Timeseries/', fileList(1).name);
-[data, ~] = read_data(img_path, 'std');
 
-s2 = size(data);
-ndvi_matrix = zeros(s2(1)*s2(2),s(1));
+ndvis = zeros(s(1),2);
+
+labels = strings(s(1),1);
 
 for i=1:s(1)
    
     img_path = strcat('data/Sentinel-2_Timeseries/', fileList(i).name);
+    labels(i) = string(fileList(i).name);
+    
     [data, ~] = read_data(img_path, 'std');
     [normalized_data, ~, ~, ~, ~] = normalize_data(data, 'min-max', 1, 0);
-    ndvi = 2*calculate_NDVI(data, [8 4]) -0.5;
     
-    ndvi_matrix(:,i) = ndvi(:);
+    cropped_data = crop(normalized_data, [100, 150, 100, 150]);
     
+    ndvi = 2*calculate_NDVI(cropped_data, [8 4]) -0.5;
+    
+    ndvis(i, 1) = max(ndvi(:));
+    ndvis(i, 2) = min(ndvi(:));
+      
 end
+
+figure, 
+x = linspace (1, s(1), s(1));
+plot(x, ndvis(:, 1), '.r', 'MarkerSize', 20); hold on; grid on;
+plot(x, ndvis(:, 2), '.g', 'MarkerSize', 20);
+
+xticklabels(labels)
+
+
